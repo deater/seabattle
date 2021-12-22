095726d0d989b8682c61806c283e42e8e7009e92
Vince Weaver <vince@deater.net>
2021-12-22 16:17:15 -0500

    some whitespace cleanup

--- binp.c
+++ binp.c
@@ -48,7 +48,7 @@ void seabattle_exit_curses(void)           /* Shutdown curses nicely so the */
    refresh();
    nocbreak();
    echo();
-   endwin();  
+   endwin();
 }
 
 
@@ -57,7 +57,7 @@ void draw_opening(void)                    /* Draw opening screen */
    int i;
    char ch;
    FILE *ftemp;
-   
+
    set_color(C_BLUE,C_BOLD);
    printxy(0,0,".|'''|                    '||'''|,            ||      ||    '||`");
    printxy(0,1,"||                         ||   ||            ||      ||     ||");
@@ -80,7 +80,7 @@ void draw_opening(void)                    /* Draw opening screen */
    set_color(C_GREEN,C_BOLD);
    printxy(18,15,"Another Amazing Project for ENEE114");
    set_color(C_WHITE,C_BOLD);
-   if (!has_colors()) { 
+   if (!has_colors()) {
       printxy(0,17,"COLOR NOT AVAILABLE ON THIS TERMINAL. Using Black and White.");
    }
    refresh();
@@ -123,12 +123,12 @@ void clear_grid(int grid[8][8])            /* Clears an 8x8 grid */
 }
 
 void do_sound(int which_one)               /* Plays appropriate sound */
-{  
+{
    if (!sound_on) return;                  /* through appropriate device */
    if (which_one==0) beep();               /* 0=error 1=hit 2=miss 3=sunkit*/
                                            /* Incomplete as of yet */
-   if (which_one==1) 
-      if (sound_device) 
+   if (which_one==1)
+      if (sound_device)
          if(rand()%2) system("cat hit1.au > /dev/audio ");
          else system("cat hit2.au > /dev/audio ");
       else beep();
@@ -136,7 +136,7 @@ void do_sound(int which_one)               /* Plays appropriate sound */
       if (sound_device)
          if(rand()%2) system("cat miss1.au > /dev/audio ");
          else system("cat miss2.au > /dev/audio ");
-   if (which_one==3) 
+   if (which_one==3)
       if (sound_device) system("cat sunkit.au > /dev/audio ");
       else beep();
 }
@@ -146,10 +146,10 @@ int get_users_grid(int grid[8][8])         /* Main function to allow user */
 {                                          /* to place ships */
    int i,ch,shiptype=0,x,y,ch2,ch3,ch4;
    char shipname[3][19]={"Submarine","Battleship","Aircraft Carrier"};
-   int ship_placed[3];     
+   int ship_placed[3];
    char text[250];
    int exit_now=0;
-  
+
    for(i=0;i<3;i++) ship_placed[i]=0;      /* Clear placement memory */
    set_color(C_RED,C_BOLD);
    printxy(32,1,"Place Your Ships");
@@ -167,11 +167,11 @@ int get_users_grid(int grid[8][8])         /* Main function to allow user */
    printxy(60,11,"   S");
    clear_grid(grid);                       /* Clear the grid */
    place_grid(grid,30,7,1);
-   
+
    do {                                    /* The big loop */
        ch=mvgetch(13,9);
-       if ( (ch>'0') && (ch<'z') ) { 
-          if ( (ch>'0') && (ch<'4') )shiptype=ch-'1';  
+       if ( (ch>'0') && (ch<'z') ) {
+          if ( (ch>'0') && (ch<'4') )shiptype=ch-'1';
           if ( (ch=='1') || (ch=='2') || (ch=='3') ){
 	     if (ship_placed[shiptype]){
 	        clear_status_lines();
@@ -214,7 +214,7 @@ int get_users_grid(int grid[8][8])         /* Main function to allow user */
 	        ch2=mvgetch(7,5);
 	        x=x/2;
 	        switch (ch2) {
-	          case 'N': case 'n': case KEY_UP : 
+	          case 'N': case 'n': case KEY_UP :
 		       if (y<shiptype+2) {
 			  simple_message(0); break;
 		       }
@@ -222,8 +222,8 @@ int get_users_grid(int grid[8][8])         /* Main function to allow user */
 		           simple_message(1); break;
 		       }
 		       else {
-			   put_ship(grid,x,y,0,-1,shiptype+3); 
-			   ship_placed[shiptype]=1;  
+			   put_ship(grid,x,y,0,-1,shiptype+3);
+			   ship_placed[shiptype]=1;
 		       }
 		       break;
 	          case 'S': case 's': case KEY_DOWN:
@@ -234,7 +234,7 @@ int get_users_grid(int grid[8][8])         /* Main function to allow user */
 		          simple_message(1); break;
 		       }
 	               else {
-			  put_ship(grid,x,y,0,1,shiptype+3); 
+			  put_ship(grid,x,y,0,1,shiptype+3);
 			  ship_placed[shiptype]=1;
 	               }
 	               break;
@@ -246,7 +246,7 @@ int get_users_grid(int grid[8][8])         /* Main function to allow user */
 		          simple_message(1); break;
 		       }
 		       else {
-			  put_ship(grid,x,y,1,0,shiptype+3); 
+			  put_ship(grid,x,y,1,0,shiptype+3);
 			  ship_placed[shiptype]=1;
 		       }
 	               break;
@@ -258,7 +258,7 @@ int get_users_grid(int grid[8][8])         /* Main function to allow user */
 	                  simple_message(1); break;
 	               }
 	               else {
-			  put_ship(grid,x,y,-1,0,shiptype+3); 
+			  put_ship(grid,x,y,-1,0,shiptype+3);
 			  ship_placed[shiptype]=1;
 	               }
 	               break;
@@ -277,7 +277,7 @@ int get_users_grid(int grid[8][8])         /* Main function to allow user */
 	       set_color(C_RED,C_BOLD);
 	       printxy(5,20,"You need to place one of each kind of ship before you continue!");
 	       do_sound(0);
-	    }	 
+	    }
          }
          if (toupper(ch)=='X') quit();
        }
@@ -309,14 +309,14 @@ void clear_side(void)                      /* Clear the side area */
    printxy(5,11,"                      ");
    printxy(5,12,"                      ");
    printxy(5,13,"                      ");
-   refresh();  
+   refresh();
 }
 
 void quit(void)                            /* Create a Quit Pop-UP */
 {
    char ch;
    WINDOW *quit_window;                    /* It's a sub-window */
-   
+
    quit_window=newwin(5,30,10,20);
    wattrset(quit_window,COLOR_PAIR(C_YELLOW)|A_BOLD);
    mvwprintw(quit_window,0,0,"*****************************");
@@ -324,7 +324,7 @@ void quit(void)                            /* Create a Quit Pop-UP */
    mvwprintw(quit_window,2,0,"* Are you sure you want to  *");
    mvwprintw(quit_window,3,0,"* Do this? (y/n)            *");
    mvwprintw(quit_window,4,0,"*****************************");
-   
+
    wrefresh(quit_window);
    ch=mvgetch(13,37);
    wclear(quit_window);
@@ -346,7 +346,7 @@ void get_computers_grid(int grid[8][8])    /* Place computers ships */
    while(place_ship(grid,4));
    while(place_ship(grid,5));
 }
-   
+
                                            /* Make sure ships don't overlap */
 int test_overlap(int grid[8][8],int x,int y,int xdir,int ydir,int size)
 {
@@ -369,7 +369,7 @@ void put_ship(int grid[8][8],int x,int y,int xdir,int ydir,int size)
 int place_ship(int grid[8][8],int size)    /* Place the computers ships */
 {                                          /* in random locations, but no */
    int x,y,direction;                      /* overlaps or out of bounds */
-   
+
    if((grid[x=rand()%8][y=rand()%8])==0){
         direction=rand()%4;
       switch(direction){
@@ -398,7 +398,7 @@ int place_ship(int grid[8][8],int size)    /* Place the computers ships */
    }
    else return -1;
 }
-   
+
                                            /* Draw very simple screen */
 void display_screen(int users_grid[8][8],int computers_grid[8][8])
 {                                          /* will be fancier with actual game */
@@ -410,16 +410,16 @@ void display_screen(int users_grid[8][8],int computers_grid[8][8])
 void purge_grid(int grid[8][8],int type)   /* Remove all of a value from */
 {                                          /* a grid */
    int i,j;
-   for(i=0;i<8;i++) for(j=0;j<8;j++) 
+   for(i=0;i<8;i++) for(j=0;j<8;j++)
      if (grid[i][j]==(type+3)) grid[i][j]=0;
 }
 
 
-void place_grid(int grid[8][8],int x,int y,int show_ships) 
+void place_grid(int grid[8][8],int x,int y,int show_ships)
 /* Display a grid on the screen */
 {
    int i,j;
-   
+
    move(y,x);
    set_color(C_WHITE,C_BOLD);
    printw("  1 2 3 4 5 6 7 8");
@@ -430,23 +430,22 @@ void place_grid(int grid[8][8],int x,int y,int show_ships)
       for(j=0;j<8;j++)
          switch(grid[j][i]){
 	  case 0: printw("  "); break;
-	  case 3: set_color(C_WHITE,C_NORMAL); 
-	          if(show_ships) printw("SS"); 
+	  case 3: set_color(C_WHITE,C_NORMAL);
+	          if(show_ships) printw("SS");
 	          else printw("  ");
 	          break;
-	  case 4: set_color(C_WHITE,C_NORMAL); 
+	  case 4: set_color(C_WHITE,C_NORMAL);
 	          if(show_ships) printw("BB");
 	          else printw("  ");
 	          break;
-	  case 5: set_color(C_WHITE,C_NORMAL); 
-	          if(show_ships) printw("AA"); 
+	  case 5: set_color(C_WHITE,C_NORMAL);
+	          if(show_ships) printw("AA");
 	          else printw("  ");
 	          break;
 	  case 1: set_color(C_RED,C_BOLD);  printw("**"); break;
-	  case 2: set_color(C_CYAN,C_NORMAL); printw("',"); 
+	  case 2: set_color(C_CYAN,C_NORMAL); printw("',");
 	 }
    }
 }
 
 /* ************************END binp.c ******************************** */
-
