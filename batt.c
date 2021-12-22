4f31cf8a65eb2e1aa28ed86b1c7cf48aac097ee7
Vince Weaver <vince@deater.net>
2021-12-22 16:00:18 -0500

    clean up whitespace
    I know, pointless churn, bugs me though

--- batt.c
+++ batt.c
@@ -8,30 +8,31 @@
 
 #include "batt.h"
 
-int sound_on=SOUND_ON;                           /*  These are exported */
-int sound_device=SOUND_SPEAKER ;                  /* Awkward, but keeps */
-int salvo_play=0;                                 /* paramaters uncluttered*/
+int sound_on=SOUND_ON;				/*  These are exported */
+int sound_device=SOUND_SPEAKER;			/* Awkward, but keeps */
+int salvo_play=0;				/* paramaters uncluttered*/
 int first_time=0;
 
 int main(int argc, char **argv) {
-   MAIN_THINGY *main_thing;
-   
-   if (( main_thing=make_main_thingy())==NULL)
-      out_of_memory_panic();
-   
-   
-   srand(time(NULL));                      /* Randomize timer */
-   init_curses();                          /* Initialize display */
-   clear();                                /* Clear the screen */
-   refresh();                          
-   draw_opening();                         /* Draw title screen */
-   clear();
-   refresh();                              
-   current_player=enter_info(main_thing);  /* Prompt to enter info */
-   
-   while (main_menu(current_player,main_thing)!=0);   /* Main Loop */
-   
-   return 0;
+
+	MAIN_THINGY *main_thing;
+
+	if (( main_thing=make_main_thingy())==NULL) {
+		out_of_memory_panic();
+	}
+
+	srand(time(NULL));			/* Randomize timer */
+	init_curses();				/* Initialize display */
+	clear();				/* Clear the screen */
+	refresh();
+	draw_opening();				/* Draw title screen */
+	clear();
+	refresh();
+	current_player=enter_info(main_thing);	/* Prompt to enter info */
+
+	while (main_menu(current_player,main_thing)!=0);   /* Main Loop */
+
+	return 0;
 }
 
 /********************* END batt.c *************************************/
